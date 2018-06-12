26e68664ad3b6f8e36e1f0a812a3f22ade1e5bad
Nam <n.truong.lt@gmail>
2018-06-12 16:03:59 +0200

    exo form double ajax, sauf que le deuxieme call ne fonctionne pas

--- src/Form/MapTownForm.php
+++ src/Form/MapTownForm.php
@@ -8,7 +8,8 @@ use Drupal\Core\Ajax\AjaxResponse;
 // use Drupal\Core\Ajax\CssCommand;
 use Drupal\Core\Ajax\HtmlCommand;
 
-class MapTownForm extends FormBase{
+class MapTownForm extends FormBase {
+
 	public function getFormID(){
 		return 'map-form';
 	}
@@ -30,6 +31,7 @@ class MapTownForm extends FormBase{
     ];
     
     $form['departements'] = [
+    	'#id' => 'departements',
       '#type' => 'select',
       '#title' => t('departements'),
       '#required' => TRUE,
@@ -38,19 +40,33 @@ class MapTownForm extends FormBase{
         
       ],
       '#access' => FALSE,
+      '#ajax' => [
+        'callback' => array($this, 'inputVillesAjax'),
+        'event' => 'change', 
+      ],
+      '#suffix' => '<span class="text-message-communes"></span>',
      ];
     
+     $form['communes'] = [
+      '#type' => 'select',
+      '#title' => t('communes'),
+      '#required' => TRUE,
+      '#description' => t('Select a communes'),
+      '#options' => [],
+      '#access' => FALSE,
+     ];
+
 
     // $form['submit'] = [
     //   '#type' => 'submit',
     //   '#value' => $this->t('expose data'),
 
     // ];
-   // ksm($form);
+   //ksm($form);
   	return $form;
 	}
   
-    public function inputDepatementAjax(array &$form, FormStateInterface $form_state){
+   public function inputDepatementAjax(array &$form, FormStateInterface $form_state){
       
       $message =  $form_state->getValue('regions');
       $response = new AjaxResponse();
@@ -62,14 +78,35 @@ class MapTownForm extends FormBase{
       $content = $out->getContents();
       $content = json_decode($content);
       foreach ($content as $data) {
-  	   $form['departements']['#options'][$data->code] = $data->nom;
-       }
+  	    $form['departements']['#options'][$data->code] = $data->nom;
+      }
 
-      $form['departement']['#access'] = TRUE;
-
-      $response->addCommand(new HtmlCommand('.text-message', $form);
+      $form['departements']['#access'] = TRUE;
+      $response->addCommand(new HtmlCommand('.text-message', $form['departements']));
       return $response;
   }
+
+  public function inputVillesAjax(array &$form, FormStateInterface $form_state){
+      
+      $message = $form_state->getValue('departements');
+
+      $response = new AjaxResponse();
+
+      $client = \Drupal::service('http_client');
+
+      $request = $client->get('https://geo.api.gouv.fr/departements/' . $message .'/communes',  ['Accept' => 'application/json']);
+      $out = $request->getBody();
+      $content = $out->getContents();
+      $content = json_decode($content);
+      foreach ($content as $data) {
+  	    $form['communes']['#options'][$data->code] = $data->nom;
+      }
+
+      $form['communes']['#access'] = TRUE;
+      $response->addCommand(new HtmlCommand('.text-message-communes', $form['communes']));
+      return $response;
+  }
+
 
   public function submitForm(array &$form, FormStateInterface $form_state){
 
